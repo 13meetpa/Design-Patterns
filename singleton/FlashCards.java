ea719875b1bf72cd27ee3478d07ad81524640f9c
Meetkumar Patel <meetkumarpatel@Meetkumars-MacBook-Air-2.local>
2024-02-10 15:01:12 -0500

    adding the java doc

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,26 +1,39 @@
 import java.util.ArrayList;
 import java.util.Random;
-
+/**
+ * The FlashCards class represents a collection of flashcards containing words
+ * It provoids functionlity to retrieve random words from the colleaction
+ */
 public class FlashCards {
     private Random rand;
     private static FlashCards flashCards;
     private ArrayList<Word> words;
     private Word currentWord;
-
+    /**
+     * Constructs a new FlashCards object with random number generator and initializes the word collection
+     * This constructor is private to enforce singleton design pattern
+     */
     private FlashCards() {
         rand = new Random();
         words = FileReader.getWords();
         currentWord = null;
     }
 
-    // Modified to be static
+    /**
+     * Retrieves the singleton instance of the FlashCards class
+     * @return The singleton instance of the FlashCards class
+     */
     public static FlashCards getInstance() {
         if (flashCards == null) {
             flashCards = new FlashCards();
         }
         return flashCards;
     }
-
+    /**
+     * Retrieves a random word from the word collection
+     * If no words are available, it prints a message and returns null
+     * @return A random Word object from the collection
+     */
     public Word getWord() {
         if (words.isEmpty()) {
             System.out.println("No words available.");
--- singleton/Word.java
+++ singleton/Word.java
@@ -1,9 +1,18 @@
+/**
+ * The Word class represents a word with its type, definition, and sentence usage
+ */
 public class Word {
     private String word;
     private String type;
     private String definition;
     private String sentence;
-
+    /**
+     * Constructs a new Word object with the specified word, type, definition, and sentence
+     * @param word The word itself
+     * @param type The type or part of speech of the word
+     * @param definition The definition of the word
+     * @param sentence  A sentence demonstrating the usage of the word
+     */
     public Word(String word, String type, String definition,String sentence ) {
 
         this.word = word;
@@ -12,10 +21,18 @@ public class Word {
         this.sentence = sentence;
 
     }
+    /**
+     * Retrieves the word
+     * @return The word
+     */
     public String getWord() {
         return word;
 
     }
+    /**
+     * Retrieves a description of the word including its type, definition, and usage in a sentence
+     * @return A formatted string containing the type, definition, and sentence
+     */
     public String getDescription() {
         return "Type: " + type + "\nDefinition: " + definition + "\nSentence: " + sentence;
     }
