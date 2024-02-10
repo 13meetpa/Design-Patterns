9515794cc2795bc95117d7028452ce5c7c66c352
Meetkumar Patel <meetkumarpatel@Meetkumars-MacBook-Air-2.local>
2024-02-09 19:05:52 -0500

    adding the code but it still doesn't work.

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,3 +1,42 @@
+import java.util.Random;
+
+import decorator.FileReader;
+
+import java.util.ArrayList;
+
 public class FlashCards {
-    
+    private Random rand;
+    private static FlashCards flashCards;
+    private ArrayList<Word> words;
+    private Word currentWord;
+
+    private FlashCards() {
+        rand = new Random();
+        words = FileReader.getWords();
+        currentWord = null;
+    }
+
+    // Modified to be static
+    public static FlashCards getInstance() {
+        if (flashCards == null) {
+            flashCards = new FlashCards();
+        }
+        return flashCards;
+    }
+
+    public Word getWord() {
+        if (words.isEmpty()) {
+            System.out.println("No words available.");
+            return null;
+        }
+
+        if (currentWord != null) {
+            words.remove(currentWord);
+        }
+
+        int randomIndex = rand.nextInt(words.size());
+        currentWord = words.get(randomIndex);
+
+        return currentWord;
+    }
 }
